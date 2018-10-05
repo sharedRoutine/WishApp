7c45f8945697c74c5f690ca5b2a7b1d7add86ef4
Janosch Hübner <sharedroutine@icloud.com>
2018-10-05 11:19:04 +0200

    Initial Commit

---
+++ WishApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WishApp
+//
+//  Created by Janosch Hübner on 05.10.18.
+//  Copyright © 2018 Janosch Hübner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
