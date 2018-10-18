11493b2a586ec2d63fe415bb31dd99892e30623f
Janosch Hübner <sharedroutine@icloud.com>
2018-10-18 18:08:02 +0200

    adds bridging header

---
+++ WishApp-Swift.h
@@ -0,0 +1,14 @@
+//
+//  WishApp-Swift.h
+//  WishApp
+//
+//  Created by Janosch Hübner on 08.10.18.
+//  Copyright © 2018 Janosch Hübner. All rights reserved.
+//
+
+#ifndef WishApp_Swift_h
+#define WishApp_Swift_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* WishApp_Swift_h */
