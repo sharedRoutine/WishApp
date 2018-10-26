56e0b0592cdf70dd36fde5fa0bf8d189f4421eaf
Janosch Hübner <sharedroutine@icloud.com>
2018-10-26 11:51:46 +0200

    adds defaultskeys for dark mode

---
+++ WishApp/Extensions/DefaultsKeysExtension.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultsKeysExtension.swift
+//  WishApp
+//
+//  Created by Janosch Hübner on 25.10.18.
+//  Copyright © 2018 Janosch Hübner. All rights reserved.
+//
+
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let darkModeEnabled = DefaultsKey<Bool>("darkModeEnabled", defaultValue: true)
+}
