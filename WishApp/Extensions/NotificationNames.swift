40dac88d034dc601c022ea2c769c45ce70eb7549
Janosch Hübner <sharedroutine@icloud.com>
2018-10-28 11:53:34 +0100

    adds notification names extension

---
+++ WishApp/Extensions/NotificationNames.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNames.swift
+//  WishApp
+//
+//  Created by Janosch Hübner on 28.10.18.
+//  Copyright © 2018 Janosch Hübner. All rights reserved.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let sortOptionDidChange: NSNotification.Name = NSNotification.Name("SortOptionDidChangeNotification")
+}
