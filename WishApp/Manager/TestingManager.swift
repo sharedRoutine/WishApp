5dfe76ff77fe2ec57a1dd9e7852c9bbe8a258340
Janosch Hübner <sharedroutine@icloud.com>
2018-10-29 16:49:17 +0100

    adds testing manager

--- WishApp/Manager/TestingManager.swift
+++ WishApp/Manager/TestingManager.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class TestingManager: NSObject {
+    public static let shared: TestingManager = TestingManager()
+    private override init() {}
+    
+    public var isTesting: Bool {
+        get {
+            return false //ProcessInfo.processInfo.arguments.contains("WISHAPP_TESTING")
+        }
+    }
+    
+    public var isTestingDataPresent: Bool {
+        get {
+            var isDirectory: ObjCBool = false
+            let testingURL: URL = Bundle.main.bundleURL.appendingPathComponent("Testing")
+            if FileManager.default.fileExists(atPath: testingURL.path, isDirectory: &isDirectory) && isDirectory.boolValue {
+                isDirectory = false
+                if FileManager.default.fileExists(atPath: testingURL.appendingPathComponent("Icons").path, isDirectory: &isDirectory) && isDirectory.boolValue {
+                    return FileManager.default.fileExists(atPath: testingURL.appendingPathComponent("Testing").appendingPathExtension("realm").path)
+                }
+            }
+            return false
+        }
+    }
+}
