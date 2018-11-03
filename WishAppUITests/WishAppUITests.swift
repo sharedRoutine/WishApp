5dd028ef399fabfa4bd8ee23f0c6d9009f3fb73a
Janosch Hübner <sharedroutine@icloud.com>
2018-11-03 18:52:27 +0100

    adds launch argument to UI test

--- WishAppUITests/WishAppUITests.swift
+++ WishAppUITests/WishAppUITests.swift
@@ -20,6 +20,7 @@ class WishAppUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         self.app = XCUIApplication()
+        self.app.launchArguments = ["WISHAPP_TESTING"]
         setupSnapshot(self.app)
         self.app.launch()
 
