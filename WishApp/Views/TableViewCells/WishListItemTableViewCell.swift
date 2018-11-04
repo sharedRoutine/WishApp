8aac4329fe7688b7ae1384b659782f0f4e6da014
Janosch Hübner <sharedroutine@icloud.com>
2018-11-04 14:18:09 +0100

    fixes price label when selected or highlighted

--- WishApp/Views/TableViewCells/WishListItemTableViewCell.swift
+++ WishApp/Views/TableViewCells/WishListItemTableViewCell.swift
@@ -124,8 +124,15 @@ class WishListItemTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        self.priceLabel.backgroundColor = UIColor.white
+    }
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+        
+        self.priceLabel.backgroundColor = UIColor.white
     }
 }
