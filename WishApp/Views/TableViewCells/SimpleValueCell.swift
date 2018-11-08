0d2b79c7e283bb0050868856dfbf520641d53cd3
Janosch Hübner <sharedroutine@icloud.com>
2018-11-08 14:40:46 +0100

    renames cell class

--- WishApp/Views/TableViewCells/SimpleValueCell.swift
+++ WishApp/Views/TableViewCells/SimpleValueCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsTableViewCell: UITableViewCell {
+class SimpleValueCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .value1, reuseIdentifier: reuseIdentifier)
