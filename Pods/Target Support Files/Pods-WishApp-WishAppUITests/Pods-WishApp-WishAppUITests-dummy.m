9ffc496400fdce020365e54094d67ea4c7c24c7c
Janosch Hübner <sharedroutine@icloud.com>
2019-01-02 19:13:17 +0100

    untracks pods

--- Pods/Target Support Files/Pods-WishApp-WishAppUITests/Pods-WishApp-WishAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WishApp_WishAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WishApp_WishAppUITests
-@end
--- Pods/Target Support Files/Pods-WishApp/Pods-WishApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WishApp : NSObject
-@end
-@implementation PodsDummy_Pods_WishApp
-@end
